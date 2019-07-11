28c8850dfa763b8980162c63811b573350714f95
Md. Maksud Alam Chowdhury <maksud.alam@iappdragon.com>
2019-07-11 18:38:30 +0600

    WIP: Base front end libraries

---
+++ client/src/RewardList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import RewardCard from "./RewardCard";
+import './assets/css/RewardList.css';
+
+class RewardList extends React.Component{
+
+  render(){
+    return (
+      <RewardCard />
+    );
+  }
+};
+
+export default RewardList;
